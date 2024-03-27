e588bf3351452e367350102c5ea426d1cd9d43b5
JonaTheBridge <jonathan@thebridgeschool.es>
2024-03-27 03:30:15 +0100

    update function class

--- 12) js/5.functions.js
+++ 12) js/5.functions.js
@@ -1,14 +1,27 @@
-function add(param1, param2, param3) {
-  return param1 + param2 + param3;
-  // console.log('post return');
+function add(parameter1, parameter2) {
+  console.log('Van a sumarse los números ' + parameter1 + ' y ' + parameter2);
+  return parameter1 + parameter2;
+  console.log('post return');
 }
 
-const arg1 = 1;
-const arg2 = 2;
-const arg3 = 3;
+const addFunction = add;
+console.log('addFunction', addFunction);
 
-const addResponse = add(arg1, arg2, arg3);
-console.log(addResponse); // 6
+const argument1 = 3;
+const argument2 = 2;
+const addResponse = add(argument1, argument2);
+console.log('addResponse', addResponse); // addResponse === 5
 
-// aplicar la función getIfIsPrime en el ejercicio de los números primos
-// esta función recibirá un número como param y devolverá un booleano
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function executeOperation(num1, num2, operation) { // operation es un callback
+  return operation(num1, num2);
+}
+
+const result1 = executeOperation(3, 2, add); // result1 === 5
+console.log('result1', result1);
+
+const result2 = executeOperation(3, 2, subtract); // result2 === 1
+console.log('result2', result2);
